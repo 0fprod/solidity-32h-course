5fa80c83799e7737e2b23599dffc6181a83a0b4f
franjpr <franpr.dev@protonmail.com>
2022-09-28 23:00:29 +0100

    verify contract programatically

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -1,7 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.8",
@@ -10,6 +13,11 @@ module.exports = {
       url: GOERLI_RPC_URL,
       accounts: [PRIVATE_KEY],
       chainId: 5,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      goerli: ETHERSCAN_API_KEY,
     },
   },
 };
--- hardhat-simple-storage/scripts/deploy.js
+++ hardhat-simple-storage/scripts/deploy.js
@@ -1,13 +1,35 @@
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 
 async function main() {
-  // const [deployer] = await ethers.getSigners();
-  // console.log("Deploying contracts with the account:", deployer.address);
-  // console.log("Account balance:", (await deployer.getBalance()).toString());
   const SimpleStorage = await ethers.getContractFactory("SimpleStorage");
   console.log("Deploying contract...");
   const simpleStorage = await SimpleStorage.deploy();
   console.log("Deployed at ", simpleStorage.address);
+
+  if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(6); // wait 6 blocks to make sure its deployed before verification
+    await verify(simpleStorage.address, []);
+  }
+}
+
+// args for the contract constructor
+async function verify(contractAddress, args) {
+  // only works for etherscan (might not work for other blockexplorers)
+  // https://docs.etherscan.io/tutorials/verifying-contracts-programmatically
+  // we can follow that guide or use a hardhat plugin
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (error) {
+    if (error.message.toLowerCase().includes("already verified")) {
+      console.log("Already verified!");
+    } else {
+      console.error(error);
+    }
+  }
 }
 
 main()
@@ -16,4 +38,3 @@ main()
     console.error(error);
     process.exit(1);
   });
-  https://youtu.be/gyMwXuJrbJQ?t=31883
