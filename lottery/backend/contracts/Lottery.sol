6e9a412df51da92ebcd102a5c40525e19e0b0d49
franjpr <franpr.dev@protonmail.com>
2022-10-18 17:32:49 +0100

    boilerplate

---
+++ lottery/backend/contracts/Lottery.sol
@@ -0,0 +1,13 @@
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+// Lottery
+// Enter the lotery (paying some amount)
+// Pick a random winner (verifiably random)
+// Winner to be selected every X minutes (automatized)
+// ChainlinkOragle -> Randomness, Automated execution (chainlink keeper)
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Lottery {
+  uint256 public foo;
+}
--- lottery/backend/hardhat.config.ts
+++ lottery/backend/hardhat.config.ts
@@ -1,17 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-etherscan";
-import "hardhat-deploy";
-import "solidity-coverage";
-import "hardhat-gas-reporter";
-import "hardhat-contract-sizer";
-import dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import "@nomicfoundation/hardhat-chai-matchers"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-deploy"
+import "hardhat-contract-sizer"
+import "hardhat-gas-reporter"
+import "solidity-coverage"
+import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config()
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
-};
+}
 
-export default config;
+export default config
